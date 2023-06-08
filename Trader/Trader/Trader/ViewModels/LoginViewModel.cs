2b28194c2d6efcb1d9fe16be219569da873c07d9
lingxi <lingxi@i-search.com.cn>
2023-06-09 00:50:47 +0800

    xxx

--- Trader/Trader/Trader/ViewModels/LoginViewModel.cs
+++ Trader/Trader/Trader/ViewModels/LoginViewModel.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Trader.Models;
 
 namespace Trader.ViewModels
 {
-    internal class LoginViewModel
+    public class LoginViewModel: ObservableObject
     {
+        static LoginRequestInfo LoginRequestInfo { get; set; }
+        public LoginViewModel() {
+        LoginRequestInfo = new LoginRequestInfo();
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return LoginRequestInfo.UserName;
+            }
+            set
+            {
+                SetProperty(ref LoginRequestInfo.UserName, value);
+            }
+        }
+        public string Password
+        {
+            get {
+                return LoginRequestInfo.Password;
+            }
+            set {
+                SetProperty(ref LoginRequestInfo.Password, value);
+            }
+        }
     }
 }
